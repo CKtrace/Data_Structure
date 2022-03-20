0af48bb42c4280d28f40387f8e45a6be1990f0eb
ChanKi Kim <97859215+CKtrace@users.noreply.github.com>
2022-03-21 00:14:34 +0900

    Update Sparsepoly.c

--- Array_Struct/Sparsepoly.c
+++ Array_Struct/Sparsepoly.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #define MAX_TERMS 101
 
-typedef struct{
+typedef struct {
 	int expon; //exponent : 지수
 	float coeff;  // coefficient : 계수
-}Term;
+} Term;
 
 typedef struct {
 	int nTerms; // 항의 개수
@@ -18,7 +18,7 @@ SparsePoly read_spoly()
 	printf("다항식의 항의 개수를 입력하시오 ");
 	scanf_s("%d", &p.nTerms);
 
-	for (int i = 0; i <p.nTerms; i++)
+	for (int i = 0; i < p.nTerms; i++)
 	{
 		printf("지수를 입력하세요! ");
 		scanf_s("%d", &p.term[i].expon);
@@ -45,16 +45,64 @@ void  printSpoly(SparsePoly a, char str[])
 	}
 }
 
-SparsePoly add_poly(SparsePoly a,SparsePoly b)
+SparsePoly add_Spoly(SparsePoly a, SparsePoly b)
 {
- // 실력 발전 후 다시 시도할 예정입니다.
+	SparsePoly p;
+	int a1 = 0; 
+	int a2 = 0; 
+	int newterm = 0;
+	int cnt = 0;
+	int i;
+	
+	newterm = a.nTerms + b.nTerms;
+
+	for (i = 0; i < newterm; i++)
+	{
+		if ((a1 >= a.nTerms) || a2 >= b.nTerms) {
+			break;
+		}
+		if (a.term[a1].expon == b.term[a2].expon)
+		{
+			p.term[i].coeff = a.term[a1].coeff + b.term[a2].coeff;
+			p.term[i].expon = a.term[a1].expon;
+			a1++;
+			a2++;
+		}
+		else if (a.term[a1].expon > b.term[a2].expon)
+		{
+			p.term[i].expon = a.term[a1].expon;
+			p.term[i].coeff = a.term[a1].coeff;
+			a1++;
+		}
+		else
+		{
+			p.term[i].expon = b.term[a2].expon;
+			p.term[i].coeff = b.term[a2].coeff;
+			a2++;
+		}
+	}
+	for (; a1 < a.nTerms; a1++)
+	{
+		p.term[i].expon = a.term[a1].expon;
+		p.term[i].coeff = a.term[a1].coeff;
+		i++;
+	}
+	for (; a2 < b.nTerms; a2++)
+	{
+		p.term[i].expon = b.term[a2].expon;
+		p.term[i].coeff = b.term[a2].coeff;
+		i++;
+	}
+	p.nTerms = i;
+
+	return p;
 }
 
-void main() 
+void main()
 {
-	SparsePoly a, b;
+	SparsePoly a, b, c;
 	a = read_spoly();
 	b = read_spoly();
-	printSpoly(a, " a = ");
-	printSpoly(b, " b = ");
+	c = add_Spoly(a, b);
+	printSpoly(c, "C = ");
 }
